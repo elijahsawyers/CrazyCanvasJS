8612289abf209261186bb7cb79aab39ef8e0fae2
Elijah Sawyers <elijahsawyers@gmail.com>
2019-03-06 15:37:36 -0600

    Setup initial files

---
+++ graphmatic.js
@@ -0,0 +1,8 @@
+function main() {
+    // Grab the PDF's iFrame, and resize the height to the size of the PDF document.
+    var pdfFrame = document.getElementById("PDFFrame");
+    
+}
+
+// Start executing after the window loads.
+window.onload = main
